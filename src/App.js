f2af912c6a0958151d213181b8c2687cd7c13d4f
ayaan <ayaanchishti444@gmail.com>
2022-09-24 13:56:13 +0530

    add list of contests

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import Editor from "./components/Editor";
 import Output from "./components/Output";
+import Contests from "./components/Contests";
 
 function App() {
   return (
     <div>
-      <Editor />
-      <Output />
+      {/* <Editor /> */}
+    <Contests></Contests>
     </div>
   );
 }
